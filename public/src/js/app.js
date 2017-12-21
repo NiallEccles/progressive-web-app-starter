036d8698c9930ea181da0957c3fe5f6e49b58cfc
Niall Eccles <nialle@rothco.ie>
2017-12-21 11:40:10 +0000

    Added promise

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator){
 	navigator.serviceWorker
 		.register('/sw.js')
@@ -5,3 +7,23 @@ if ('serviceWorker' in navigator){
 			console.log('Service worker registered.');
 		});
 }
+
+window.addEventListener('beforeinstallprompt', function(){
+	console.log('beforeinstallprompt fired');
+	event.preventDefault();
+	deferredPrompt = event;
+	return false;
+});
+
+var promise = new Promise(function(resolve, reject){
+	setTimeout(function(){
+		resolve('This is executed once the timeout is done');
+	//console.log('This is executed once the timeout is done');
+	}, 3000);
+});
+
+promise.then(function(text){
+	console.log(text);
+});
+
+console.log('This is executed after timeout');
