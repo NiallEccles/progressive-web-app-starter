0979decdd6f437439f9ea0959c27686cf552d7aa
Niall Eccles <n.eccles14@gmail.com>
2018-04-25 21:45:29 +0100

    sw versoning names now more robust

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,9 @@
+const CACHE_STATIC_NAME = 'static-v4';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';	
 self.addEventListener('install', function(event){
 	console.log('[Service Worker] Installing Service Worker ...', event);
 	event.waitUntil(
-		caches.open('static-v2')
+		caches.open(CACHE_STATIC_NAME)
 			.then(function(cache){
 				console.log('[Service Worker] Precaching app shell')
 				cache.addAll([
@@ -29,7 +31,7 @@ self.addEventListener('activate', function(event){
 		caches.keys()
 			.then(function(keyList){
 				return Promise.all(keyList.map(function(key){
-					if(key !== 'static-v2' &&  key !== 'dynamic'){
+					if(key !== CACHE_STATIC_NAME &&  key !== CACHE_DYNAMIC_NAME){
 						console.log('Removing old cache', key);
 						return caches.delete(key);
 					}
@@ -48,7 +50,7 @@ self.addEventListener('fetch', function(event){
 			} else{
 				return fetch(event.request)
 					.then(function(res){
-						return caches.open('dynamic')
+						return caches.open(CACHE_DYNAMIC_NAME)
 							.then(function(cache){
 								cache.put(event.request.url, res.clone())
 								return res;
